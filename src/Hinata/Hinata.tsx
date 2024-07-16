c2474ae6d3ac7af660c6bcc513e1b8fe4bacb7d2
Fibo <fibonshin@gmail.com>
2024-07-16 20:09:38 +0900

    react-iconを追加

--- src/Hinata/Hinata.tsx
+++ src/Hinata/Hinata.tsx
@@ -1,13 +1,25 @@
 import './Hinata.css';
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import { IoMdHome } from "react-icons/io";
 
 function Hinata() {
+  const navigate=useNavigate();
   return (
     <div className='hinata'>
-      <p>HINATA</p>
-      <Link to="/">Home</Link>
-      <br />
-      <Link to="/hinata">hinata</Link>
+      <div className="homeButton">
+        <Link to="/">
+          <IoMdHome color='black' size='50px' />
+        </Link>
+      </div>
+      <div className="title">
+        <br />
+        <p>日向坂46</p>
+        <p>ペンライト</p>
+        <p>QUIZ</p>
+      </div>
+      <button className='btn1' onClick={()=> navigate('/')}>メンバー➔ペンライトカラー<br />QUIZ</button>
+      <button className='btn1' onClick={()=> navigate('/')}>ペンライト➔カラーメンバー<br />4択QUIZ</button>
+      <button className='btn1' onClick={()=> navigate('/')}>メンバー➔ペンライトカラー<br />QUIZ</button>
     </div>
   );
 }
